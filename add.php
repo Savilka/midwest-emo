32e050b0542e283502e6e15345d9913e9aae45c8
Savilka <simanmore@yandex.ru>
2021-11-03 18:30:00 +0700

    make it better

--- add.php
+++ add.php
@@ -1,5 +1,6 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "midwestemo");
+$mysqli = new mysqli("n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "t3yx98bakhvb6qcu",
+    "x220h26wnon03a7t", "msf3ai2ttoov1rtw");
 if ($mysqli->connect_errno) {
     echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
@@ -8,7 +9,7 @@ $text = $_POST["text"];
 $announce = $_POST["announce"];
 $pic = $_POST["pic"];
 $timestamp = date('Y-m-d H:i:s');
-$err = $mysqli->query("INSERT INTO midwestemo.news (date, headline, announce, text, pic) VALUE ('$timestamp', '$headline', '$announce',
+$err = $mysqli->query("INSERT INTO `news` (date, headline, announce, text, pic) VALUE ('$timestamp', '$headline', '$announce',
                                                                         '$text', '$pic')");
 header('Location: news.php', false);
 
--- delete_news.php
+++ delete_news.php
@@ -1,8 +1,9 @@
 <?php
 $id = (int)$_GET['id'];
-$mysqli = new mysqli("localhost", "root", "", "midwestemo");
+$mysqli = new mysqli("n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "t3yx98bakhvb6qcu",
+    "x220h26wnon03a7t", "msf3ai2ttoov1rtw");
 if ($mysqli->connect_errno) {
     echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-$results = $mysqli->query("DELETE FROM midwestemo.news WHERE id = '$id'");
+$results = $mysqli->query("DELETE FROM `news` WHERE id = '$id'");
 header('Location: news.php', false);
