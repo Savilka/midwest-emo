24cc519c2682e4403b5226e1e9eafda25f594382
Savilka <simanmore@yandex.ru>
2021-11-03 23:22:44 +0700

    make it better

--- login.php
+++ login.php
@@ -1,7 +1,5 @@
 <?php
 ob_start();
 session_start();
-
 $_SESSION['admin'] = 1;
-
 header('Location: index.php', false);
--- unlogin.php
+++ unlogin.php
@@ -1,7 +1,5 @@
 <?php
 ob_start();
 session_start();
-
 $_SESSION['admin'] = 0;
-
 header('Location: index.php', false);
