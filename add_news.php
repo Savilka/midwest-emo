d6e022fe0231a3c8e2e024c38b490e7cf43ea290
Savilka <simanmore@yandex.ru>
2021-11-09 13:55:20 +0700

    delete viewport

--- add_news.php
+++ add_news.php
@@ -5,7 +5,6 @@ session_start();
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Добавить новость</title>
     <link rel="stylesheet" href="styles/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
