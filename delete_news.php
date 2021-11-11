661610a93b1ba1c3f50fa6eb189c55c08fe47f1b
Savilka <73016265+Savilka@users.noreply.github.com>
2021-11-11 15:56:51 +0700

    Update delete_news.php

--- delete_news.php
+++ delete_news.php
@@ -1,7 +1,7 @@
 <?php
 $id = (int)$_GET['id'];
 $mysqli = new mysqli("n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "t3yx98bakhvb6qcu",
-    "x220h26wnon03a7t", "msf3ai2ttoov1rtw");
+    "qd7hagdyc9mp44g5 ", "msf3ai2ttoov1rtw");
 if ($mysqli->connect_errno) {
     echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
